446b129d9147f16d27b44df514fc97659896cb9d
Michel Osee <michel.osee@ulb.be>
2021-02-15 12:05:08 +0100

    Ajout des projets liés au voltmètre

---
+++ exercices/voltmetreTemplate.X/main.c
@@ -0,0 +1,21 @@
+/*
+ * Main file of voltmetreTemplate.X
+ * Author: mosee
+ *
+ * Template de projet utilisant la libraire de l'ADC
+ */
+#define FCY 3685000     // µcycle frequency. Needed for __delay_ms
+#include "libpic30.h"   // Contains __delay_ms definition
+#include "xc.h"
+#include "adc.h"
+
+
+
+int main(void) {
+    // Configurations et initialisations
+    
+    // Boucle principale
+	while(1) {
+        
+	}
+}
