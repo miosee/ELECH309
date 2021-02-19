489d3fb413e91f1b5b12b78955d5262fac908fc5
Michel Osee <michel.osee@ulb.be>
2021-02-19 14:35:47 +0100

    Ajout du projet oscilloscope

--- exercices/oscilloscope/oscilloscope.X/main.c
+++ exercices/oscilloscope/oscilloscope.X/main.c
@@ -1,23 +1,24 @@
 /*
- * Main file of echo.X
+ * Main file of voltmetre2.X
  * Author: mosee
  *
- * Projet servant à tester une connexion UART.
- * RB6 sert de patte Rx du dsPIC et RB7 sert de patte Tx.
- * L'UART du dsPIC est configuré en 8N1 à 57.6 kb/s
- * Le dsPIC implémente un simple écho : il écoute son Rx et renvoie tout ce 
- * qu'il reçoit sur son Tx.
+ * Implémentation du voltmètre en utilisant le timer3. La fréquence
+ * d'échantillonnage est la plus précise possible pour le dsPIC, car
+ * les conversions sont entièrement gérées par le hardware.
  */
 
 #include "xc.h"
 #include "adc.h"
 
 
-
 int main(void) {
-    unsigned char c;
-    
-    /* Configuration du Peripheral Pin Select (PPS) pour connecter le signal
+    unsigned int sample, i;
+   
+    // Configuration de L'ADC pour utilisation en polling sur AN0
+    adcInit(ADC_TIMER3_SAMPLING);
+    PR3 = 772;                 // T=10ms=(PR1+1)/3.685MHz => PR1=36849
+
+        /* Configuration du Peripheral Pin Select (PPS) pour connecter le signal
      * Rx de l'UART1 à RB6/RP6 et le signal Tx à RB7/RP7 */
 	_U1RXR = 6;    // U1RX -> RP6
 	_RP7R = 3;     // RP7 -> U1Tx
@@ -36,14 +37,20 @@ int main(void) {
     
     U1MODEbits.UARTEN = 1;      // on active l'UART
     U1STAbits.UTXEN = 1;        // on active l'émission
-    
-    // Boucle principale
+
+
 	while(1) {
         if (U1STAbits.URXDA) {      // si UART1 a reçu un octet
-            c = U1RXREG;                // on le lit
-            while (U1STAbits.UTXBF) {}  // on attend que le buffer d'émission soit libre
-            U1TXREG = c;                // et on le renvoie
+            sample = U1RXREG;
+            if (sample == 's') {
+            	T3CONbits.TON = 1;
+                for (i=0; i<1000; i++) {
+                    while (adcConversionDone()) {}
+                    sample = adcRead()/4;
+                    while (U1STAbits.UTXBF) {}
+                    U1TXREG = sample;
+                }
+            }
         }
-        
 	}
 }
